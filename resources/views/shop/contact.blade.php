6f477f5388850316672cf6e3518afcd8ef690de6
Zihad <zihadnayem8@gmail.com>
2021-01-17 17:17:51 +0600

    added front page layout and also added admin_dashboard layout.

---
+++ resources/views/shop/contact.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.master')
+
+@section('content')
+    <div class="col-lg-12">
+
+        <h1 class="my-4">Contact</h1>
+        <p class="text-muted">Contact us text</p>
+
+    </div>
+    <!-- /.col-lg-12 -->
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('shop.index');
 });
+
+Route::get('/about', function (){
+    return view('shop.about');
+})->name('about');
+Route::get('/service', function (){
+    return view('shop.service');
+})->name('service');
+Route::get('/contact', function (){
+    return view('shop.contact');
+})->name('contact');
+Route::get('/admin-dashboard', function (){
+    return view('admin.index');
+})->name('admin');
+
+
+
